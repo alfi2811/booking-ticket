a68dd63cf98dc653f7b15f31d3293c2a2523eb66
alfi2811 <alfinasution64@gmail.com>
2021-09-17 09:15:45 +0700

    feature mvc

---
+++ server.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+type User struct {
+	ID    int    `json:"id" form:"id"`
+	Name  string `json:"name" form:"name"`
+	Email string `json:"email" form:"email"`
+}
+
+func initDB() {
+	dsn := "root:@tcp(127.0.0.1:3306)/booking_ticket?charset=utf8mb4&parseTime=True&loc=Local"
+	var err error
+	DB, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic(err.Error())
+	}
+	initMigrate()
+}
+
+func initMigrate() {
+	DB.AutoMigrate(&User{})
+}
+
+func main() {
+	initDB()
+	e := echo.New()
+	e.GET("/", func(c echo.Context) error {
+		return c.String(http.StatusOK, "Hello, World!")
+	})
+	e.Logger.Fatal(e.Start(":1323"))
+}
